0ef73d2863b3478b3405056e723ba7c62b51d5ee
Magnusdaroe <pt05wz@student.aau.dk>
2024-07-22 11:04:05 +0200

    f

---
+++ src/drone/scripts/Parse.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import serial
+from pymavlink import mavutil
+
+class MavlinkReaderNode(Node):
+
+    def __init__(self):
+        super().__init__('mavlink_reader_node')
+        self.publisher_ = self.create_publisher(String, 'mavlink_messages', 10)
+        self.serial_port = '/dev/ttyTHS1'
+        self.baud_rate = 115200
+        self.timer = self.create_timer(0.1, self.timer_callback)
+        self.ser = serial.Serial(self.serial_port, self.baud_rate)
+        self.mavlink_connection = mavutil.mavlink_connection(self.ser)
+        self.get_logger().info(f"Connected to {self.serial_port} at {self.baud_rate} baud")
+
+    def timer_callback(self):
+        msg = self.mavlink_connection.recv_match(blocking=True)
+        if msg:
+            mavlink_msg = String()
+            mavlink_msg.data = str(msg)
+            self.publisher_.publish(mavlink_msg)
+            self.get_logger().info(f"Published MAVLink message: {mavlink_msg.data}")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MavlinkReaderNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
