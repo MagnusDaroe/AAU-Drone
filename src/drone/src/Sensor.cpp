bd1f30467ae74853a4ac302b9b9a84a3096f9ea8
Magnusdaroe <pt05wz@student.aau.dk>
2024-07-29 15:26:43 +0200

    Update Sensor.cpp

--- src/drone/src/Sensor.cpp
+++ src/drone/src/Sensor.cpp
@@ -13,7 +13,27 @@ class SerialReader : public rclcpp::Node
 {
 public:
     SerialReader()
-    : Node("Sensor_node")
+    : Node("Sensor_node"),
+      ahrs_tag_map({
+          {"acc_x", &drone_interfaces::msg::SensorData::acc_x},
+          {"acc_y", &drone_interfaces::msg::SensorData::acc_y},
+          {"acc_z", &drone_interfaces::msg::SensorData::acc_z},
+          {"gyro_x", &drone_interfaces::msg::SensorData::gyro_x},
+          {"gyro_y", &drone_interfaces::msg::SensorData::gyro_y},
+          {"gyro_z", &drone_interfaces::msg::SensorData::gyro_z},
+          {"mag_x", &drone_interfaces::msg::SensorData::mag_x},
+          {"mag_y", &drone_interfaces::msg::SensorData::mag_y},
+          {"mag_z", &drone_interfaces::msg::SensorData::mag_z},
+          {"altitude", &drone_interfaces::msg::SensorData::altitude}
+      }),
+      gps_tag_map({
+          {"lat", &drone_interfaces::msg::SensorData::lat},
+          {"lon", &drone_interfaces::msg::SensorData::lon},
+          {"time", &drone_interfaces::msg::SensorData::time},
+          {"speed", &drone_interfaces::msg::SensorData::speed},
+          {"pdop", &drone_interfaces::msg::SensorData::pdop},
+          {"hdop", &drone_interfaces::msg::SensorData::hdop}
+      })
     {
         serial_port_ = "/dev/ttyUSB0"; 
         baud_rate_ = 115200;
@@ -43,28 +63,8 @@ public:
     }
 
 private:
-    std::unordered_map<std::string, double drone_interfaces::msg::SensorData::*> ahrs_tag_map = {
-        {"acc_x", &drone_interfaces::msg::SensorData::acc_x},
-        {"acc_y", &drone_interfaces::msg::SensorData::acc_y},
-        {"acc_z", &drone_interfaces::msg::SensorData::acc_z},
-        {"gyro_x", &drone_interfaces::msg::SensorData::gyro_x},
-        {"gyro_y", &drone_interfaces::msg::SensorData::gyro_y},
-        {"gyro_z", &drone_interfaces::msg::SensorData::gyro_z},
-        {"mag_x", &drone_interfaces::msg::SensorData::mag_x},
-        {"mag_y", &drone_interfaces::msg::SensorData::mag_y},
-        {"mag_z", &drone_interfaces::msg::SensorData::mag_z},
-        {"altitude", &drone_interfaces::msg::SensorData::altitude}
-    };
-
-    std::unordered_map<std::string, double drone_interfaces::msg::SensorData::*> gps_tag_map = {
-        {"lat", &drone_interfaces::msg::SensorData::lat},
-        {"lon", &drone_interfaces::msg::SensorData::lon},
-        {"time", &drone_interfaces::msg::SensorData::time},
-        {"speed", &drone_interfaces::msg::SensorData::speed},
-        {"pdop", &drone_interfaces::msg::SensorData::pdop},
-        {"hdop", &drone_interfaces::msg::SensorData::hdop}
-    };
-
+    std::unordered_map<std::string, double drone_interfaces::msg::SensorData::*> ahrs_tag_map;
+    std::unordered_map<std::string, double drone_interfaces::msg::SensorData::*> gps_tag_map;
     std::unordered_map<std::string, std::shared_ptr<std::unordered_map<std::string, std::string>>(SerialReader::*)(const std::string&)> sensor_tag_map;
 
     void read_sensor()
